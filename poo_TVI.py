5ade36793c3d2eabc46b28cbfd5d08a81dfacf5e
olivier-be <61296476+olivier-be@users.noreply.github.com>
2021-11-27 00:17:12 +0100

    Update poo_TVI.py

--- poo_TVI.py
+++ poo_TVI.py
@@ -1,15 +1,27 @@
 from math import sqrt
 class TVI:
-    def __init__(self,a,puissance_a,b,puissance_b,c,puissance_c,valeur_chercher):
-        self._tabpuissance=[puissance_a,puissance_b,puissance_c]
-        self._tababc=[a,b,c]
-        self._tababc_prime = [self._tababc*self._tabpuissance[i] for i in range(3)]
-        self._puissance_prime=[self._tabpuissance[i]-1 for i in range(3)]
+    def __init__(self,a,puissance_a,b,puissance_b,c,puissance_c,d,puissance_d,valeur_chercher):
+        self._tabpuissance=[puissance_a,puissance_b,puissance_c,puissance_d]
+        self._tababc=[a,b,c,d]
+        self._tababc_prime = [self._tababc[i]*self._tabpuissance[i] for i in range(4)]
+        self._tabpuissance_prime=[self._tabpuissance[i] - 1 for i in range(4)]
         self._TVI=False
-        self.r1=0
-        self.r2=0
+        self._r1=0
+        self._r2=0
         self._chr=valeur_chercher
         self._signe_segmenttab= ["0"] * 3
+        self.z=[puissance_a,puissance_b,puissance_c,puissance_d]
+        self._tab_variation=["0"]*3
+        """ while self._tabpuissance==self.z:
+            for i in range(len(self._tabpuissance)-1):
+                if self._tabpuissance[i]>self._tabpuissance[i+1]:
+                    temp=self._tabpuissance[i+1]
+                    tempp=self._tababc[i+1]
+                    self._tabpuissance[i+1]=self._tabpuissance[i]
+                    self._tababc[i + 1]=self._tababc[i]
+                    self._tababc[i]=tempp
+                    self._tabpuissance[i]=temp
+                """
 
     def __eq__(self,a):
         if type(a)==bool:
@@ -17,25 +29,34 @@ class TVI:
         else:
             assert 1==2,("l'égaliter comparar un bool a un ",type(a))
     def __str__(self):
-        if TVI==True:
-            return("True")
+        self.TVI()
+        if self._TVI==True:
+            print("True TVI")
+            return("True TVI")
+
         else :
+            print("TVI False")
             return("TVI False")
 
     def racine(self):
-        if self._tabpuissance[0]>0 or self._tabpuissance[1]>0 or self._tabpuissance[2]>0 :
-            d=self._tababc[1]*self._tababc[1]-(4*self._tababc[0]*self._tababc[2])
+        if self._tabpuissance_prime[0]>0 or self._tabpuissance_prime[1]>0 or self._tabpuissance_prime[2]>0 or self._tabpuissance_prime[3]>0:
+            d=self._tababc_prime[1]*self._tababc_prime[1]-(4*self._tababc_prime[0]*self._tababc_prime[2])
+            print(d)
             if d>0:
-                self._r1=(-self._tababc[1]-sqrt(d))/(2*self._tababc[0])
-                self._r2 = (-self._tababc[1]+sqrt(d)) / (2 * self._tababc[0])
-            if d == 0:
-                self._r1 = (-self._tababc[1] - sqrt(d)) / (2 * self._tababc[0])
+                self._r1=(-self._tababc_prime[1]-sqrt(d))/(2*self._tababc_prime[0])
+                self._r2 = (-self._tababc_prime[1]+sqrt(d)) / (2 * self._tababc_prime[0])
+                print((-self._tababc_prime[1]+sqrt(d))/ (2 * self._tababc_prime[0]))
+            elif d == 0:
+                self._r1 = (-self._tababc_prime[1] - sqrt(d)) / (2 * self._tababc_prime[0])
                 self._r2 = self._r1
+            print("racine", self._r1, self._r2)
+
 
     def signe_segment(self):
         z=self.cathegorie_segment()
-        for i in range(0, 2, 2):
-            if self._tababc[i] > 0:
+        for i in range(0, 3):
+            print(i)
+            if self._tababc[0] > 0:
                 self._signe_segmenttab[i] = "+"
             else:
                 self._signe_segmenttab[i] = "-"
@@ -48,6 +69,13 @@ class TVI:
                 self._signe_segmenttab[1] ="-"
             else:
                 self._signe_segmenttab[1] ="+"
+    def tab_variation(self):
+        for i in range (len(self._signe_segmenttab)):
+            if self._signe_segmenttab[i]=="+":
+                self._tab_variation[i]="croissant"
+            else:
+                self._tab_variation[i] = "decroissant"
+
 
     def cathegorie_segment (self):
         self.racine()
@@ -62,16 +90,19 @@ class TVI:
         return self._tababc[0]*inconue**self._tabpuissance[0]+self._tababc[1]*inconue**self._tabpuissance[1]+self._tababc[2]*inconue**self._tabpuissance[2]
 
     def TVI(self):
+        self.signe_segment()
         comp=0
         print("on sait que:")
         print("f est continue car c'est un polynome")
-        if self._chr>self.fonction(self.r1):
-            print("le nombre a pour intervalle]",self._signe_segmenttab[0],"+inf;",self.r1,"]")
+        if self._chr>self.fonction(self._r1):
+            print("le nombre a pour intervalle]", self._signe_segmenttab[0],"+inf;", self._r1, "]")
             comp+=1
-        elif self._chr>self.fonction(self.r2):
-            print("le nombre a pour intervalle[", self.r2,";",self._signe_segmenttab[2],"inf[")
+        elif self._chr>self.fonction(self._r2):
+            print("le nombre a pour intervalle[", self._r2, ";", self._signe_segmenttab[2], "inf[")
             comp += 1
-        elif self._chr>self.fonction(self.r1) and self._chr<self.fonction(self.r2):
-            print("le nombre a pour intervalle[", self.r1, ";", self.r2,"]")
+        elif self._chr>self.fonction(self._r1) and self._chr<self.fonction(self._r2):
+            print("le nombre a pour intervalle[", self._r1, ";", self._r2, "]")
             comp += 1
-        print("la valeur apprait {} de fois",format(comp))
+        print("la valeur apprait {} fois".format(comp))
+        if comp>0:
+            self._TVI=True
