42aebe2724d6e0fe4d9085e07bbfe84d11128de0
olivier-be <61296476+olivier-be@users.noreply.github.com>
2021-11-27 14:21:15 +0100

    Update poo_TVI.py

--- poo_TVI.py
+++ poo_TVI.py
@@ -1,21 +1,24 @@
 from math import sqrt
 class TVI:
-    def __init__(self,a,puissance_a,b,puissance_b,c,puissance_c,d,puissance_d,valeur_chercher,intervalle1,intervalle2,intervalleac):
-        self._tabpuissance=[puissance_a,puissance_b,puissance_c,puissance_d]
+    def __init__(self, a, power_x_of_a, b, power_x_of_b, c, power_x_of_c, d, power_x_of_d, valeur_search, interval1, interval2, intervalac,langue):
+        self._tabpower=[power_x_of_a, power_x_of_b, power_x_of_c, power_x_of_d]
         self._tababcd=[a, b, c, d]
-        self._tababcd_prime = [self._tababcd[i] * self._tabpuissance[i] for i in range(4)]
-        self._tabpuissance_prime=[self._tabpuissance[i] - 1 for i in range(4)]
+        self._tababcd_prime = [self._tababcd[i] * self._tabpower[i] for i in range(4)]
+        self._tabpower_prime=[self._tabpower[i] - 1 for i in range(4)]
         self._TVI=False
         self._r1=0
         self._r2=0
-        self._chr=valeur_chercher
+        self._shr=valeur_search
         self._signe_segmenttab= ["0"] * 3
-        self.z=[puissance_a,puissance_b,puissance_c,puissance_d]
+        self.z=[power_x_of_a, power_x_of_b, power_x_of_c, power_x_of_d]
         self._tab_variation=["0"]*3
-        self._degre=puissance_a
-        self.intervalle=intervalle1
-        self.intervalle2=intervalle2
-        self.intervalleb=intervalleac
+        self._degre=power_x_of_a
+        self._interval=interval1
+        self._interval2=interval2
+        self._intervalb=intervalac
+        self._languefr=["on sait que:","f est continue car c'est un polynome","le nombre a pour intervalle ]","la valeur apprait {} fois","l'égaliter comparar un type TVI a un ","racine","1 racine","deux racine distincte"]
+        self._langueus=["we know that:","f is continuous because it was an polynomail","the number search have for interval","the value appear {} time","the egalty compared TVI to","root","1 root","whitout root","two root distinct"]
+        self._languechoice=["a","a","a","a","a","a""a","a","a","a","a","a","a""a","a","a","a"]
         """ while self._tabpuissance==self.z:
             for i in range(len(self._tabpuissance)-1):
                 if self._tabpuissance[i]>self._tabpuissance[i+1]:
@@ -26,13 +29,17 @@ class TVI:
                     self._tababc[i]=tempp
                     self._tabpuissance[i]=temp
                 """
+        if langue=="FR"or langue=="fr":
+            self._languechoice=self._languefr
+        elif langue=="US"or langue=="UK"or langue=="us"or langue=="uk":
+            self._languechoice=self._langueus
 
     def __eq__(self,a):
         if type(a)==TVI:
             #if a.
             return True
         else:
-            assert 1==2,("l'égaliter comparar un type TVI a un ",type(a))
+            assert 1==2,(self._languechoice[4],type(a))
     def __str__(self):
         self.TVI()
         if self._TVI==True:
@@ -43,8 +50,8 @@ class TVI:
             print("TVI False")
             return("TVI False")
 
-    def racine(self,puissance,abcd):
-        if puissance[0]>0 or puissance[1]>0 or puissance[2]>0 or puissance[3]>0:
+    def root(self, power, abcd):
+        if power[0]>0 or power[1]>0 or power[2]>0 or power[3]>0:
             d= abcd[1] * abcd[1] - (4 * abcd[0] * abcd[2])
             print(d)
             if d>0:
@@ -54,10 +61,10 @@ class TVI:
             elif d == 0:
                 self._r1 = (-abcd[1] - sqrt(d)) / (2 * abcd[0])
                 self._r2 = self._r1
-            print("racine", self._r1, self._r2)
+            print(self._languechoice[6], self._r1, self._r2)
 
 
-    def signe_segment(self):
+    def sign_segment(self):
         z=self.cathegorie_segment()
         for i in range(0, 3):
             print(i)
@@ -65,11 +72,11 @@ class TVI:
                 self._signe_segmenttab[i] = "+"
             else:
                 self._signe_segmenttab[i] = "-"
-        if z==("1 racine"):
+        if z==(self._languechoice[5]):
             self._signe_segmenttab[1]="0"
-        elif z==("sans racine"):
+        elif z==(self._languechoice[6]):
             self._signe_segmenttab[1] =self._signe_segmenttab[0]
-        elif z==("deux racines distinctes"):
+        elif z==(self._languechoice[7]):
             if self._signe_segmenttab[0] == "+":
                 self._signe_segmenttab[1] ="-"
             else:
@@ -84,38 +91,38 @@ class TVI:
 
     def cathegorie_segment (self):
         if self._degre==3:
-            self.racine(self._tabpuissance_prime,self._tababcd_prime)
+            self.root(self._tabpower_prime, self._tababcd_prime)
         elif self._degre==2:
-            self.racine( self._tabpuissance,self._tababcd)
+            self.root(self._tabpower, self._tababcd)
         if self._r1==self._r2:
-            return("1 racine")
+            return(self._languechoice[5])
         elif self._r1!=self._r2 and self._tababcd[1]*self._tababcd[1]-(4 * self._tababcd[0] * self._tababcd[2])<0:
-            return ("sans racine")
+            return (self._languechoice[6])
         else:
-            return("deux racines distinctes")
+            return(self._languechoice[7])
 
-    def fonction(self,inconue):
-        return self._tababcd[0] * inconue ** self._tabpuissance[0] + self._tababcd[1] * inconue ** self._tabpuissance[1] + self._tababcd[2] * inconue ** self._tabpuissance[2]
+    def fonction(self, x):
+        return self._tababcd[0] * x ** self._tabpower[0] + self._tababcd[1] * x ** self._tabpower[1] + self._tababcd[2] * x ** self._tabpower[2]
 
     def TVI(self):
-        self.signe_segment()
+        self.sign_segment()
         comp=0
-        print("on sait que:")
-        print("f est continue car c'est un polynome")
-        if (self._chr<=self.fonction(self._r1) and self._signe_segmenttab[0]=="-" and (self.intervalle<self._r1)or self.intervalleb==False) or (self._chr>=self.fonction(self._r1) and self._signe_segmenttab[0]=="+" and (self.intervalle>self._r1)or self.intervalleb==False):
-            print("le nombre a pour intervalle ]",self._signe_segmenttab[0],"inf;", self._r1, "]")
+        print(self._languechoice[0])
+        print(self._languechoice[1])
+        if (self._shr <= self.fonction(self._r1) and self._signe_segmenttab[0] == "-" and (self._interval < self._r1) or self._intervalb == False) or (self._shr >= self.fonction(self._r1) and self._signe_segmenttab[0] == "+" and (self._interval > self._r1) or self._intervalb == False):
+            print(self._languechoice[2],self._signe_segmenttab[0],"inf;", self._r1, "]")
             comp+=1
-        if  (self._chr<=self.fonction(self._r2) and self._signe_segmenttab[2]=="-"and (self.intervalle<self._r2)or self.intervalleb==False) or (self._chr>=self.fonction(self._r2) and self._signe_segmenttab[2]=="+" and (self.intervalle>self._r2)or self.intervalleb==False):
-            print("le nombre a pour intervalle[ ",self._r2, ";", self._signe_segmenttab[2], "inf[")
+        if  (self._shr <= self.fonction(self._r2) and self._signe_segmenttab[2] == "-" and (self._interval < self._r2) or self._intervalb == False) or (self._shr >= self.fonction(self._r2) and self._signe_segmenttab[2] == "+" and (self._interval > self._r2) or self._intervalb == False):
+            print(self._languechoice[2],self._r2, ";", self._signe_segmenttab[2], "inf[")
             comp += 1
-        if self.fonction(self._r1)< self.fonction(self._r2) and (((self.intervalle<self._r1)and ((self.intervalle<self._r2)))or self.intervalleb==False):
-            if self._chr>=self.fonction(self._r1) and self._chr<=self.fonction(self._r2):
-                print("le nombre a pour intervalle[ ",self._r1, ";", self._r2, "]")
+        if self.fonction(self._r1)< self.fonction(self._r2) and (((self._interval < self._r1) and ((self._interval < self._r2))) or self._intervalb == False):
+            if self._shr>=self.fonction(self._r1) and self._shr<=self.fonction(self._r2):
+                print(self._languechoice[2] ,self._r1, ";", self._r2, "]")
                 comp += 1
         else:
-            if self._chr>=self.fonction(self._r2) and self._chr<=self.fonction(self._r1) and (((self.intervalle>self._r1)and ((self.intervalle>self._r2)))or self.intervalleb==False):
-                print("le nombre a pour intervalle[ ",self._r1, ";", self._r2, "]")
+            if self._shr>=self.fonction(self._r2) and self._shr<=self.fonction(self._r1) and (((self._interval > self._r1) and ((self._interval > self._r2))) or self._intervalb == False):
+                print(self._languechoice[2] ,self._r1, ";", self._r2, "]")
                 comp += 1
-        print("la valeur apprait {} fois".format(comp))
+        print(self._languechoice[3].format(comp))
         if comp>0:
             self._TVI=True
